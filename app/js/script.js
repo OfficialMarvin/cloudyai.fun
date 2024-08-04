d76221a35489a95da50a0338de288d94661214a7
Marvin Jakobs <38141141+OfficialMarvin@users.noreply.github.com>
2024-08-04 10:15:52 -0400

    Update script.js

--- app/js/script.js
+++ app/js/script.js
@@ -2,13 +2,6 @@ const navbarShowBtn = document.querySelector('.navbar-show-btn');
 const navbarHideBtn = document.querySelector('.navbar-hide-btn');
 const navbarListWrapper = document.querySelector('.navbar-list-wrapper');
 
-navbarShowBtn.addEventListener('click', () => {
-    navbarListWrapper.classList.add('show');
-});
-
-navbarHideBtn.addEventListener('click', () => {
-    navbarListWrapper.classList.remove('show');
-});
 
 let resizeTimer;
 window.addEventListener('resize', () => {
